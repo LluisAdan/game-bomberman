f020b010dd089e87809092e7eeaca702b6638740
Lluis Adan <lluis.adan@gmail.com>
2024-01-28 21:58:05 +0100

    Feat: styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', () => {
-    /*
     const startGameBtn = document.getElementById('btn-start-game')
     startGameBtn.addEventListener('click', () => {
         const game = new Game('main-canvas')
@@ -15,6 +14,65 @@ window.addEventListener('load', () => {
         canvasPanel.classList.remove('hidden')
 
         game.start()
+    })
+
+    const selectMapBtn = document.getElementById('btn-choose-bg')
+    selectMapBtn.addEventListener('click', () => {
+        const mapPanel = document.getElementById('map-panel')
+        mapPanel.classList.remove('hidden')
+
+        const startPanel = document.getElementById('start-panel')
+        startPanel.classList.add('hidden')
+    })
+
+    const mapOne = document.getElementById('btn-map-1')
+    mapOne.addEventListener('click', () => {
+        selectionBackground(1)
+    })
+
+    const mapTwo = document.getElementById('btn-map-2')
+    mapTwo.addEventListener('click', () => {
+        selectionBackground(2)
+    })
+
+    const mapThree = document.getElementById('btn-map-3')
+    mapThree.addEventListener('click', () => {
+        selectionBackground(3)
+    })
+
+    const mapFour = document.getElementById('btn-map-4')
+    mapFour.addEventListener('click', () => {
+        selectionBackground(4)
+    })
+
+    const mapFive = document.getElementById('btn-map-5')
+    mapFive.addEventListener('click', () => {
+        selectionBackground(5)
+    })
+
+    const mapSix = document.getElementById('btn-map-6')
+    mapSix.addEventListener('click', () => {
+        selectionBackground(6)
+    })
+
+    const panelPj = document.getElementById('btn-choose-pj')
+    panelPj.addEventListener('click', () => {
+        const imgPj = document.getElementById('img-pjs')
+        if (imgPj.classList.value.includes('hidden')) {
+            imgPj.classList.remove('hidden')
+        } else {
+            imgPj.classList.add('hidden')
+        }
+    })
+
+    const difficult = document.getElementById('btn-difficult')
+    difficult.addEventListener('click', () => {
+        const btnDif = document.getElementById('difficult')
+        if (btnDif.classList.value.includes('hidden')) {
+            btnDif.classList.remove('hidden')
+        } else {
+            btnDif.classList.add('hidden')
+        }
     })
     
     const gameOverYes = document.getElementById('btn-yes')
@@ -39,11 +97,12 @@ window.addEventListener('load', () => {
         startPanel.classList.remove('hidden')
         window.location.reload()
     })
-    */
     
+    /*
     const game = new Game('main-canvas')
     document.addEventListener('keydown', (event) => game.onKeyEvent(event))
     document.addEventListener('keyup', (event) => game.onKeyEvent(event))
     game.start()
+    */
     
 })
--- assets/js/models/background.js
+++ assets/js/models/background.js
@@ -16,6 +16,9 @@ class Background {
     }
 
     draw() {
+        
+        this.selectionBackground(1)
+
         if (this.bg.isReady) {
             this.ctx.drawImage(
                 this.bg,
@@ -26,4 +29,52 @@ class Background {
             )
         }
     }
+
+    selectionBackground(selectionMap) {
+        switch (selectionMap) {
+            case 1:
+                this.bg = new Image()
+                this.bg.src = '/assets/img/bg/bg-1.png'
+                this.bg.onload = () => {
+                    this.bg.isReady = true
+                }
+                break
+            case 2:
+                this.bg = new Image()
+                this.bg.src = '/assets/img/bg/bg-2.png'
+                this.bg.onload = () => {
+                    this.bg.isReady = true
+                }
+                break
+            case 3:
+                this.bg = new Image()
+                this.bg.src = '/assets/img/bg/bg-3.png'
+                this.bg.onload = () => {
+                    this.bg.isReady = true
+                }
+                break
+            case 4:
+                this.bg = new Image()
+                this.bg.src = '/assets/img/bg/bg-4.png'
+                this.bg.onload = () => {
+                    this.bg.isReady = true
+                }
+                break
+            case 5:
+                this.bg = new Image()
+                this.bg.src = '/assets/img/bg/bg-5.png'
+                this.bg.onload = () => {
+                    this.bg.isReady = true
+                }
+                break
+            case 6:
+                this.bg = new Image()
+                this.bg.src = '/assets/img/bg/bg-6.png'
+                this.bg.onload = () => {
+                    this.bg.isReady = true
+                }
+                break
+        }
+    }
+
 }
